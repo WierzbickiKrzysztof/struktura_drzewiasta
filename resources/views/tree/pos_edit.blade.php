e3743de404b890b9087fc2cd7e24122d4d60d5b2
Krzysztof Wierzbicki <10692955+WierzbickiKrzysztof@users.noreply.github.com>
2023-04-26 00:46:36 +0200

    Fix view

--- resources/views/tree/pos_edit.blade.php
+++ resources/views/tree/pos_edit.blade.php
@@ -1,6 +1,6 @@
 <x-main>
     <h2>Ustaw kolejność</h2>
-    <h3>Pozycje nie mogą się powtarzać</h3>
+    <h5>Pozycje nie mogą się powtarzać</h5>
         <form method="POST" id="editForm" action="/tree/pos/{{ request()->route('id') }}" >
             <div class="modal-body">
 
@@ -11,34 +11,24 @@
                 @csrf
                 @method('PUT')
 
-                @php $poz=1; @endphp
+                @php($poz=1)
                 @foreach ($pos as $key => $value)
 
                 <div class="form-group">
                     <label for="position[{{ $poz }}]">Pozycja {{ $poz }}:</label>
                     <select class="form-control" id="position[{{ $poz }}]" name="position[{{ $poz }}]">
                         @foreach ($pos as $key => $value)
-                        <option value="{{ $key }}">[{{ $key }}]{{ $value }}</option>
+                        <option value="{{ $key }}">[{{ $key }}] - {{ $value }}</option>
                         @endforeach
                     </select>
                 </div>
-                    @php $poz++; @endphp
+                    @php($poz++)
                 @endforeach
-
-
-
-                {{--                    <div class="form-group">--}}
-                {{--                        <label for="position_edit">Pozycja:</label>--}}
-                {{--                        <select class="form-control" id="position_edit" name="position">--}}
-                {{--                            <option value="">-- placeholder --</option>--}}
-                {{--                        </select>--}}
-                {{--                    </div>--}}
-
 
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" onclick="history.back()">Wróć</button>
-                <button type="submit" class="btn btn-primary">Edytuj</button>
+                <button type="submit" class="btn btn-primary">Zapisz</button>
             </div>
         </form>
 
